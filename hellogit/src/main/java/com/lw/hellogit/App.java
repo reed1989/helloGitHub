6f216c8a1ffc9773b8b7260234b7d6e24f065446
reed1989 <371252146@qq.com>
2018-10-31 22:50:02 +0800

    add a new class

--- hellogit/src/main/java/com/lw/hellogit/App.java
+++ hellogit/src/main/java/com/lw/hellogit/App.java
@@ -1,11 +1,21 @@
 package com.lw.hellogit;
 
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public void say(String something)
+    {
+        if (null != something || something.length() != 0)
+        {
+            System.out.println(something);
+        }
+    }
+    
+    
     public static void main( String[] args )
     {
         System.out.println("Hello git hub!");
---
+++ hellogit/src/main/java/com/lw/hellogit/Printer.java
@@ -0,0 +1,11 @@
+package com.lw.hellogit;
+
+public class Printer
+{
+
+    public Printer()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
